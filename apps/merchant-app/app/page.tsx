08ba8446a41027b7f751127aa1513caa1f88cf8b
kenilJoshi <keniljoshi3@gmail.com>
2024-09-27 12:35:17 +0530

    first commit

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -5,6 +5,6 @@ import { useBalance } from "@repo/store/balance";
 export default function() {
   const balance = useBalance();
   return <div>
-    hi there how are you {balance}
+    hi there how are {balance}
   </div>
 }
